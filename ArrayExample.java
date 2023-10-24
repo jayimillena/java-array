bf27cff625b4585ffa448a68613f16548bd380ad
Two <22344029+jayimillena@users.noreply.github.com>
2023-10-24 11:30:04 +0800

    Initial commit

---
+++ ArrayExample.java
@@ -0,0 +1,20 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Declare and initialize an integer array with 5 elements
+        int[] myArray = new int[5];
+
+        // Initialize the elements of the array
+        myArray[0] = 10;
+        myArray[1] = 20;
+        myArray[2] = 30;
+        myArray[3] = 40;
+        myArray[4] = 50;
+
+        // Access and print elements of the array
+        System.out.println("Element at index 0: " + myArray[0]);
+        System.out.println("Element at index 1: " + myArray[1]);
+        System.out.println("Element at index 2: " + myArray[2]);
+        System.out.println("Element at index 3: " + myArray[3]);
+        System.out.println("Element at index 4: " + myArray[4]);
+    }
+}
